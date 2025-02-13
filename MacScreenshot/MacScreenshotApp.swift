930f95e0d19e266720616fcc7aa425a4c77ab96d
アウン <hlumaungphyo@gmail.com>
2025-02-14 02:29:33 +0900

    Initial Commit

---
+++ MacScreenshot/MacScreenshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacScreenshotApp.swift
+//  MacScreenshot
+//
+//  Created by Hlwan Aung Phyo on 2025/02/14.
+//
+
+import SwiftUI
+
+@main
+struct MacScreenshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
