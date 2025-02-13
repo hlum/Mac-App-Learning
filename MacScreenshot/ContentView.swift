39eb5046a20b1b105ccedacd716eea73016383e9
アウン <hlumaungphyo@gmail.com>
2025-02-14 02:43:42 +0900

    take screenshot btn added and show the screenshot in the window

--- MacScreenshot/ContentView.swift
+++ MacScreenshot/ContentView.swift
@@ -8,14 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var image: NSImage?
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if let image = image {
+                Image(nsImage: image)
+                    .resizable()
+                    .scaledToFit()
+            }
+            
+            Button {
+                let task = Process()
+                task.executableURL = URL(fileURLWithPath: "/usr/sbin/screencapture")
+                task.arguments = ["-cw"] //copy , window
+                do {
+                    try task.run()
+                    task.waitUntilExit()
+                    self.image = getImageFromPasteboard()
+                } catch {
+                    print(error.localizedDescription)
+                }
+            } label: {
+                Text("Make a screenshot")
+            }
+
         }
         .padding()
+    }
+    
+    func getImageFromPasteboard() -> NSImage? {
+        // Check if the copied item on the pasteBoard is an Image
+        guard NSPasteboard.general.canReadItem(withDataConformingToTypes: NSImage.imageTypes)
+        else {return nil }
+        
+        // Take out the image from the paste board
+        return NSImage(pasteboard: NSPasteboard.general)
     }
 }
 
